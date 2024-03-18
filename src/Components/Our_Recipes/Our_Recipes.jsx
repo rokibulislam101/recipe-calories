e32b6b00ec6270f0e7961e3420ab4e7a6e390883
MD Rokibul Islam Mollah <rokibulislamalways101@gmail.com>
2024-03-18 11:52:37 +0600

    Final Commit

--- src/Components/Our_Recipes/Our_Recipes.jsx
+++ src/Components/Our_Recipes/Our_Recipes.jsx
@@ -3,8 +3,8 @@ const Our_Recipes = () => {
     <main className="flex justify-center items-center flex-col text-center space-y-5 my-20 mx-20">
       <h1 className="text-5xl font-bold">Our Recipes</h1>
       <p className="md:w-1/2 text-lg ">
-        Over 150,000 parents enjoy our weekly newsletter, with informative blog
-        posts, product information, and more!
+        Join over 100,000 parents who delight in our weekly newsletter packed
+        with insightful blog entries and a wealth of additional resources tailored just for you!
       </p>
     </main>
   );
